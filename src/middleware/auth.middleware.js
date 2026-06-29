6be97e8cc9170bb091e2e01d98c0975c4007cb8c
ShobhitLala <shobhitsrivastava1001@gmail.com>
2026-06-29 14:18:17 +0530

    feat:debug

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -7,7 +7,14 @@ export const authenticate= async(req,res,next)=>{
     return res.status(401).json({
         message: "Unauthorized"
     });}
-    const token = authHeader.split(" ")[1];
+   const parts = authHeader.split(" ");
+
+console.log("Parts:", parts);
+console.log("Parts length:", parts.length);
+
+const token = parts[1];
+
+console.log("Extracted token:", token);
      const decoded=jwt.verify(token,process.env.JWT_SECRET);
      const user = await User.findById(decoded.userId)
     .select("-password");
